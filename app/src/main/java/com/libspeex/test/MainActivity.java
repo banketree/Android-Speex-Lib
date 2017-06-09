9d6b03721e49317595aba8aa3d2580c20fd9ca59
banketree <banketree@qq.com>
2017-06-09 10:18:59 +0800

    测试完成

--- app/src/main/java/com/libspeex/test/MainActivity.java
+++ app/src/main/java/com/libspeex/test/MainActivity.java
@@ -1,16 +1,21 @@
 package com.libspeex.test;
 
+import android.Manifest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.libspeex.test.helper.AudioPlayer;
 import com.libspeex.test.helper.AudioRecorder;
+import com.tbruyelle.rxpermissions.RxPermissions;
 import com.thinkcore.storage.TFilePath;
 import com.thinkcore.utils.log.TLog;
 
 import java.io.File;
+
+import rx.functions.Action1;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private AudioRecorder mAudioRecorder;
@@ -36,22 +41,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.Button_start_record) {
-            TFilePath filePath = new TFilePath();
-            mVolumeStartTime = System.currentTimeMillis();
-            mVolumeFilePath = filePath.getExternalCacheDir() + File.separator
-                    + mVolumeStartTime + AudioRecorder.Field_Suffix;
 
-            if (mAudioRecorder != null)
-                mAudioRecorder.stopRecord(new AudioRecorder.RecorderListener() {
+            RxPermissions.getInstance(this).request(Manifest.permission.RECORD_AUDIO)
+                    .subscribe(new Action1<Boolean>() {
+                        @Override
+                        public void call(Boolean granted) {
+                            if (granted) {
+                                TFilePath filePath = new TFilePath();
+                                mVolumeStartTime = System.currentTimeMillis();
+                                mVolumeFilePath = filePath.getExternalCacheDir() + File.separator
+                                        + mVolumeStartTime + AudioRecorder.Field_Suffix;
 
-                    @Override
-                    public void onStop(String filePath) {
+                                if (mAudioRecorder != null)
+                                    mAudioRecorder.stopRecord(new AudioRecorder.RecorderListener() {
 
-                    }
-                });
-
-            mAudioRecorder = new AudioRecorder(mVolumeFilePath);
-            mAudioRecorder.startRecord();
+                                        @Override
+                                        public void onStop(String filePath) {
+                                            TLog.i("", filePath);
+                                        }
+                                    });
+                                mAudioRecorder = new AudioRecorder(mVolumeFilePath);
+                                mAudioRecorder.startRecord();
+                            } else {
+                                Toast.makeText(MainActivity.this, "没有权限", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         } else if (view.getId() == R.id.Button_stop_record) {
             if (mAudioRecorder == null)
                 return;
@@ -60,7 +75,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
                 @Override
                 public void onStop(String filePath) {
-
+                    TLog.i("", filePath);
                 }
             });
         } else if (view.getId() == R.id.Button_start_audio) {
@@ -71,6 +86,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             try {
                 AudioPlayer.getInstance().startPlay(mVolumeFilePath, null);
             } catch (Exception e) {
+                e.printStackTrace();
             }
         } else if (view.getId() == R.id.Button_stop_audio) {
             AudioPlayer.getInstance().stopPlayer();
---
+++ app/src/main/java/com/libspeex/test/MyApplication.java
@@ -0,0 +1,10 @@
+package com.libspeex.test;
+
+import com.thinkcore.TApplication;
+
+/**
+ * Created by Administrator on 2017/6/9.
+ */
+
+public class MyApplication extends TApplication {
+}
